13ab246a980044c5be216ffee0c15862cd8f6dd3
bengyphan <bengyphan@gmail.com>
2020-01-20 20:27:16 -0800

    finished yelpcamp v1

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const commentRoutes = require("./routes/comments"),
       indexRoutes = require("./routes/index")
 
 
-// mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect("mongodb+srv://Admin:Charizard@93@cluster0-myicn.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
