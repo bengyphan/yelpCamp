7031c8051c4a1ea65e54fcdd92de4af53fbea188
bengyphan <bengyphan@gmail.com>
2020-01-20 19:54:55 -0800

    added mongodb atlas

--- app.js
+++ app.js
@@ -17,9 +17,8 @@ const commentRoutes = require("./routes/comments"),
       indexRoutes = require("./routes/index")
 
 
-mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true, useUnifiedTopology: true});
-// mongoose.connect("mongodb+srv://Admin:Charizard@93@cluster0-myicn.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
-// mongodb+srv://Admin:<password>@cluster0-myicn.mongodb.net/test?retryWrites=true&w=majority
+// mongoose.connect("mongodb://localhost:27017/yelpcamp", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://Admin:Charizard@93@cluster0-myicn.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
